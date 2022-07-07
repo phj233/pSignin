b89d598cdf07df3a2d9b2486a87d35b6a3db09d2
phj233 <2780990934@qq.com>
2022-07-07 13:45:32 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+function result(result){
+    return result;
+}
+class pSignin extends NIL.ModuleBase{
+    onStart(api){  
+        api.logger.info('psignin加载成功');
+        var signinList = new Array(300);
+        api.listen('onMainMessageReceived',(e)=>{
+            if(e.raw_message=='签到'){
+                if(NIL._vanilla.wl_exists(e.sender.qq)){
+                    var pl = NIL._vanilla.get_xboxid(e.sender.qq);
+                    var money=Math.round(Math.random()*1001+10);
+                    let ser = NIL.SERVERS.get('BDS');
+                    var mem=e.sender.qq;
+                    let res = signinList.some(qq => qq === mem)
+                    if(res!=true){
+                        signinList.unshift(mem);
+                        ser.sendCMD(`money add ${pl} ${money}`,result);
+                        e.reply(`恭喜<-${pl}->获得"${money}"个金币，上线看看⑧`,`AT`);
+                    }else{
+                        e.reply(`你今天已经签到过了，令人感叹`,`AT`);
+                    }
+
+                }
+                else{
+                    e.reply(`你未添加绑定Xbox白名单，请绑定后再试`,`AT`);
+                }
+            }
+        });
+        var id =setInterval(function () {
+            if (new Date().getMinutes()==59 &&new Date().getHours()==23&&new Date().getSeconds()==58){
+                signinList.length=0;
+            }
+        },1000,id)
+    }
+    onStop(){
+
+    }
+}
+
+module.exports = new pSignin;
